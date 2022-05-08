f7d374a3724a6a32efa054d16a962ce0e9572e57
jprekz <jprekz@users.noreply.github.com>
2022-05-08 17:56:41 +0900

    remove gvimrc

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -s -b ~/dotfiles/.vimrc ~/.vimrc
-ln -s -b ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s -b ~/dotfiles/.bashrc ~/.bashrc
 ln -s -b ~/dotfiles/.inputrc ~/.inputrc
 ln -s -b ~/dotfiles/.gitconfig ~/.gitconfig
