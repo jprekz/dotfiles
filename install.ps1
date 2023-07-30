dca0a15b1fe0d39b036c4351c056c077bcbf2fbe
jprekz <jprekz@users.noreply.github.com>
2023-07-30 21:21:34 +0900

    commit

--- install.ps1
+++ install.ps1
@@ -1,10 +1,15 @@
 # admin only
 
+# runas (https://qiita.com/sakekasunuts/items/63a4023887348722b416)
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) {
+    Start-Process pwsh.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit
+}
+
 $files = @(
     ".vimrc"
     ".bashrc"
     ".inputrc"
-    ".gitconfig"
+    ".gitconfig.sync"
 )
 
 $here = Split-Path $PSCommandPath -Parent 
@@ -12,3 +17,10 @@ $here = Split-Path $PSCommandPath -Parent
 foreach ($file in $files) {
     New-Item -Path (Join-Path $home $file) -Value (Join-Path $here $file) -ItemType SymbolicLink
 }
+
+Add-Content (Join-Path $home ".gitconfig") @'
+[include]
+    path = ~/.gitconfig.sync
+'@
+
+read-host “Press ENTER to continue...”
--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
 
-for file in .vimrc .bashrc .inputrc .gitconfig
+for file in .vimrc .bashrc .inputrc .gitconfig.sync
 do
   if [ -e ~/$file ]; then
     mv ~/$file ~/${file}.origin
   fi
   ln -s $script_dir/$file ~/$file
 done
+
+cat << EOS >> ~/.gitconfig
+[include]
+    path = ~/.gitconfig.sync
+EOS
